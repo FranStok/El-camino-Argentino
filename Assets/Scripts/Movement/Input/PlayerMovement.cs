cd90d5c17e511fc872a4e5d8acc58121c80dc272
F-Astoreca <fastoreca@sinergiasoluciones.com.ar>
2025-07-24 21:47:30 -0300

    salto medio pocho

--- Assets/Scripts/Movement/Input/PlayerMovement.cs
+++ Assets/Scripts/Movement/Input/PlayerMovement.cs
@@ -5,16 +5,19 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] float moveSpeed = 2;
-    [SerializeField] float jumpSpeed = 1;
+    [SerializeField] float jumpHeight = 3f;
 
     InputAction moveAction;
     InputAction jumpAction;
 
-    CharacterController CharacterController;
+    CharacterController characterController;
+
+    float verticalSpeed;
 
     void Start()
     {
-        CharacterController = GetComponent<CharacterController>();
+        verticalSpeed = 0;
+        characterController = GetComponent<CharacterController>();
         moveAction = InputSystem.actions.FindAction("move");
         jumpAction = InputSystem.actions.FindAction("jump");
     }
@@ -22,13 +25,27 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Vector2 moveValue = moveAction.ReadValue<Vector2>() * Time.deltaTime * moveSpeed;
+        calculateVerticalSpeed();
         MovePlayer(moveValue);
+    }
+
+    void calculateVerticalSpeed()
+    {
+        if (characterController.isGrounded)
+        {
+            verticalSpeed = (jumpAction.triggered) ? Mathf.Sqrt(Physics.gravity.y * jumpHeight * -2) : -1;
+            Debug.Log("SALTO"); 
+            Debug.Log(verticalSpeed); 
+            return;
+        }
+            Debug.Log("NO SALTO"); 
+        verticalSpeed += Physics.gravity.y * Time.deltaTime; //La gravedad es negativa
     }
 
     void MovePlayer(Vector2 moveVector)
     {
-        Vector3 move = transform.forward * moveVector.y + transform.right * moveVector.x;
-        CharacterController.Move(move);
+        Vector3 move = transform.forward * moveVector.y + transform.right * moveVector.x + Vector3.up * verticalSpeed;
+        characterController.Move(move);
     }
 
 
